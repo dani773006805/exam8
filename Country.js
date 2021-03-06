06e0d5d5263b6b1ca6dae65ed4be9c7e4b807de3
Dani <dani>
2021-03-06 17:22:49 +0600

    adding function creating country element

---
+++ Country.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function createCountry(countries) {
+    let country = countries[0];
+    let countryElement = document.createElement('div');
+    countryElement.classList.add('countryElement');
+    countryElement.innerHTML =
+        '<img class="card-img-top" src="' + country.flag + '"' + 'alt="' + country.name + '">' +
+        '<h5>' + country.name + '</h5>' +
+        '<ul class="list-group list-group-flush">' +
+        '<li class="list-group-item">Capital: ' + country.capital + '</li>' +
+        '<li class="list-group-item">Region: ' + country.region + '</li>' +
+        '<li class="list-group-item">Currency: ' + country.currencies[0].name + '</li>' +
+        '</ul>' +
+        '<a class="btn btn-light btn-outline-info" href="https://www.google.com/search?q=' + country.name + '" target="_blank">Google more</a>' +
+        '<a class="btn btn-light btn-outline-info" href="https://ru.m.wikipedia.org/wiki/' + country.name + '" target="_blank">Wikipedia more</a>';
+    return countryElement;
+
+}
